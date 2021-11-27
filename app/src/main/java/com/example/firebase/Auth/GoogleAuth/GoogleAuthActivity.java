2142404a476628a363f1b1c9ccc34b20e0bb7b5e
Ashish Sharma <ak47.jks@gmail.com>
2021-11-27 18:50:28 +0530

    verision 1.0 second commit

--- app/src/main/java/com/example/firebase/Auth/GoogleAuth/GoogleAuthActivity.java
+++ app/src/main/java/com/example/firebase/Auth/GoogleAuth/GoogleAuthActivity.java
@@ -67,9 +67,6 @@ public class GoogleAuthActivity extends AppCompatActivity {
         updateUI(currentUser);
     }
 
-
-
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
